5d707076c54600ae7ea90b56fa4c928159657ba1
sickSamurai <ldmckkb@gmail.com>
2023-03-10 14:28:26 -0500

    sign-in service working. bugs with auth utils

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -4,67 +4,41 @@ import { map, Observable, ReplaySubject, tap } from 'rxjs';
 import { User } from 'app/core/user/user.types';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class UserService
-{
-    private _user: ReplaySubject<User> = new ReplaySubject<User>(1);
+export class UserService {
+  private _user: ReplaySubject<User> = new ReplaySubject<User>(1);
 
-    /**
-     * Constructor
-     */
-    constructor(private _httpClient: HttpClient)
-    {
-    }
+  constructor(private _httpClient: HttpClient) { }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Accessors
-    // -----------------------------------------------------------------------------------------------------
+  set user(value: User) {
+    // Store the value
+    this._user.next(value);
+  }
 
-    /**
-     * Setter & getter for user
-     *
-     * @param value
-     */
-    set user(value: User)
-    {
-        // Store the value
-        this._user.next(value);
-    }
+  get user$(): Observable<User> {
+    return this._user.asObservable();
+  }
 
-    get user$(): Observable<User>
-    {
-        return this._user.asObservable();
-    }
+  /**
+   * Get the current logged in user data
+   */
+  get(): Observable<User> {
+    return this._httpClient.get<User>('api/common/user').
+      pipe(tap((user) => {
+        this._user.next(user);
+      }));
+  }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * Get the current logged in user data
-     */
-    get(): Observable<User>
-    {
-        return this._httpClient.get<User>('api/common/user').pipe(
-            tap((user) => {
-                console.log(user);
-                this._user.next(user);
-            })
-        );
-    }
-
-    /**
-     * Update the user
-     *
-     * @param user
-     */
-    update(user: User): Observable<any>
-    {
-        return this._httpClient.patch<User>('api/common/user', {user}).pipe(
-            map((response) => {
-                this._user.next(response);
-            })
-        );
-    }
+  /**
+   * Update the user
+   *
+   * @param user
+   */
+  update(user: User): Observable<any> {
+    return this._httpClient.patch<User>('api/common/user', { user }).
+      pipe(map((response) => {
+        this._user.next(response);
+      }));
+  }
 }
